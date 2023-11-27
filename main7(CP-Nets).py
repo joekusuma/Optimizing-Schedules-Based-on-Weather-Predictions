5c5bf5702e6ae3612d180b481ae76cab5c68c028
Jamestilfords <117862249+Jamestilfords@users.noreply.github.com>
2023-11-26 19:34:09 -0500

    Update main7(CP-Nets).py

--- main7(CP-Nets).py
+++ main7(CP-Nets).py
@@ -4,6 +4,66 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.colors as mcolors
+import pandas as pd
+import requests
+
+# Constants and Global Variables
+api_key = "28c300a25e5245d6ba3223640231511"
+location = "Los Angeles"
+
+# Function to Fetch and Process Weather Data for a Specific Day
+def fetch_weather_data(api_key, location, selected_date):
+    url = f"http://api.weatherapi.com/v1/history.json?key={api_key}&q={location}&dt={selected_date}"
+    response = requests.get(url)
+    data = response.json()
+
+    df_weather = pd.DataFrame()
+    for hour in data['forecast']['forecastday'][0]['hour']:
+        df_weather = df_weather.append({
+            'date': selected_date,
+            'time': hour['time'],
+            'temp_c': hour['temp_c'],
+            'wind_kph': hour['wind_kph'],
+            'humidity': hour['humidity'],
+            'chance_of_rain': hour['chance_of_rain'],
+            'precip_mm': hour['precip_mm'],
+            'vis_km': hour['vis_km'],
+        }, ignore_index=True)
+
+    df_weather['datetime'] = pd.to_datetime(df_weather['time'])
+    return df_weather
+
+def interpret_weather_score(chance_of_rain, preferred_weather):
+    # Example interpretation: lower score if chance of rain is high but preference is sunny
+    if preferred_weather == "Sunny":
+        return 3 if chance_of_rain < 20 else 1
+    elif preferred_weather == "Cloudy":
+        return 2
+    else:  # Rainy
+        return 1 if chance_of_rain > 70 else 3
+
+def adjust_preferences_based_on_weather(activity_name, start_time, duration, preferred_weather, weather_data):
+    end_time = start_time + timedelta(hours=duration)
+    average_weather_score = 0
+    count = 0
+
+    # Calculate average weather preference score for the activity duration
+    while start_time < end_time:
+        hour_weather = weather_data.get(start_time.hour)
+        if hour_weather is not None and not hour_weather.empty:
+            # Example: Score based on chance of rain
+            chance_of_rain = hour_weather['chance_of_rain']
+            # Use .iloc[0] if 'chance_of_rain' is a Series
+            if isinstance(chance_of_rain, pd.Series):
+                chance_of_rain = chance_of_rain.iloc[0]
+            weather_score = interpret_weather_score(chance_of_rain, preferred_weather)
+            average_weather_score += weather_score
+            count += 1
+        start_time += timedelta(hours=1)
+
+    if count > 0:
+        return average_weather_score / count
+    return {"Sunny": 3, "Cloudy": 2, "Rainy": 1}[preferred_weather]  # Default to initial preference if no weather data
 
 # Function for plotting the activity timeline
 def plot_activity_timeline(schedule, planning_day):
@@ -39,7 +99,7 @@ def add_activity_input(key):
     return {"name": activity_name, "duration": duration, "weather": weather_preference}
 
 # Function implementing CP-Net logic
-def CPNet(activities, start_datetime, end_datetime):
+def CPNet(activities, weather_data, start_datetime, end_datetime):
     preferences = {}
     conditions = {}
 
@@ -48,12 +108,20 @@ def CPNet(activities, start_datetime, end_datetime):
         activity_duration = activity["duration"]
         activity_weather = activity["weather"]
 
-        preferences[activity_name] = {"Sunny": 3, "Cloudy": 2, "Rainy": 1}[activity_weather]
+        # Initial preference based on user input
+        initial_pref = {"Sunny": 3, "Cloudy": 2, "Rainy": 1}[activity_weather]
+
+        # Adjust preferences based on weather data
+        adjusted_pref = adjust_preferences_based_on_weather(activity_name, start_datetime, activity_duration, activity_weather, weather_data)
+
+        preferences[activity_name] = adjusted_pref
         conditions[activity_name] = {"duration": activity_duration, "time_range": (start_datetime, end_datetime)}
 
+    # Generate all possible permutations of activities
     activity_names = [activity["name"] for activity in activities]
     all_permutations = list(permutations(activity_names))
-
+    
+    # Find the best schedule based on preferences and conditions
     best_schedule = None
     best_score = -float("inf")
 
@@ -112,7 +180,11 @@ def user_interface():
         start_datetime = datetime.combine(planning_day, start_time)
         end_datetime = datetime.combine(planning_day, end_time)
 
-        best_schedule = CPNet(activities, start_datetime, end_datetime)
+        # Fetch weather data
+        weather_data = fetch_weather_data(api_key, location, planning_day.strftime("%Y-%m-%d"))
+        hourly_weather_data = {pd.to_datetime(row['time']).hour: row for index, row in weather_data.iterrows()}
+
+        best_schedule = CPNet(activities, hourly_weather_data, start_datetime, end_datetime)
 
         if best_schedule:
             st.write("Optimized Schedule:")
@@ -124,10 +196,9 @@ def user_interface():
             st.pyplot(fig)
         else:
             st.write("No feasible schedule found.")
-            
+
 def main():
     user_interface()
 
 if __name__ == "__main__":
     main()
-
